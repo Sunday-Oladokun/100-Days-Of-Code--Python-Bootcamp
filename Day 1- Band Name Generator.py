03abfbaa55e09ef9b06e663c3b3e986586ac98b9
Black-Concept <anthonyoladokun@gmail.com>
2024-04-02 22:14:33 +0100

    Day 1: Band Name Generator
    02-04-2024

---
+++ Day 1- Band Name Generator.py
@@ -0,0 +1,12 @@
+# DAY 1: BAND NAME GENERATOR PROGRAM
+
+# Introduce the program
+print("Welcome to the Band Name Generator, this program takes the name of the city you grew up and your pet's name to generate a band name for you")
+
+# Request for city name
+street = input("What's the name of the city you grew up in?\n")
+# Request for pet name
+pet = input("What's your pet's name?\n")
+
+# Print out the band name for the user
+print("Your band name could be " + street + " " + pet)
